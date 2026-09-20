a8443ae252a395473e396fc5a7303c954e19095c
Vincentajor <ajorvincent20@gmail.com>
2026-09-20 20:43:53 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ const TELEGRAM_PROFILE_URL =
 
 const LOGIN_CREDENTIALS = {
   id: "8703579203",
-  password: "Lizz_prvt.1",
+  password: "Zoo2505vip",
 };
 
 /* =========================================================
